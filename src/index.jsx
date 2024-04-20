b798dfb7051f446137a370ca90ee5274d5db640e
Petra Smejkalova <smejkalova.petra@gmail.com>
2024-04-20 19:15:56 +0200

    BrowserRouter

--- src/index.jsx
+++ src/index.jsx
@@ -1,28 +1,24 @@
 import React from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  BrowserRouter,
+  Route,
+  Routes,
+  RouterProvider,
+} from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import './global.css';
 import { HomePage } from './pages/HomePage';
 import { ProductPage } from './pages/ProductPage';
 import { App } from './components/app/App';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        index: true,
-        element: <HomePage />,
-      },
-      {
-        path: 'product/:productId',
-        element: <ProductPage />,
-      },
-    ],
-  },
-]);
-
 createRoot(document.querySelector('#app')).render(
-  <RouterProvider router={router} />,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="" element={<HomePage />} />
+        <Route path="product/:id" element={<ProductPage />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>,
 );
